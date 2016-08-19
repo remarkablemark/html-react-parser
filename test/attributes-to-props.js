b2ba8518cd06959b6a0fd9300856bcf2cd096daf
Mark <mark@remarkablemark.org>
2016-08-19 13:34:04 -0400

    Use the React property config when converting attributes to props
    - Update `attributeToProps` to use the React property config.
    - Refactor the helper and keep things functional (do not mutate
      the original attributes object).
    - Add tests that check that certain DOM attributes and properties
      are properly converted to React props.

--- test/attributes-to-props.js
+++ test/attributes-to-props.js
@@ -11,6 +11,62 @@ var attributesToProps = require('../lib/attributes-to-props');
  */
 describe('attributes to props helper', function() {
 
+    it('converts DOM attributes to React props', function() {
+        assert.deepEqual(
+            attributesToProps({
+                'class': 'ic',
+                'for': 'tran',
+                'http-equiv': 'refresh'
+            }),
+            {
+                className: 'ic',
+                htmlFor: 'tran',
+                httpEquiv: 'refresh',
+            }
+        );
+    });
+
+    it('converts DOM standard properties to React props', function() {
+        assert.deepEqual(
+            attributesToProps({
+                allowfullscreen: true,
+                charset: 'utf-8',
+                tabindex: 1
+            }),
+            {
+                allowFullScreen: true,
+                charSet: 'utf-8',
+                tabIndex: 1
+            }
+        );
+    });
+
+    it('converts DOM RDFa properties to React props', function() {
+        assert.deepEqual(
+            attributesToProps({
+                property: 'foo',
+                'typeof': 'bar'
+            }),
+            {
+                property: 'foo',
+                'typeof': 'bar'
+            }
+        );
+    });
+
+    it('converts DOM non-standard properties to React props', function() {
+        assert.deepEqual(
+            attributesToProps({
+                itemscope: true,
+                itemid: 1337
+            }),
+            {
+                itemScope: true,
+                itemID: 1337
+            }
+        );
+    });
+
     it('converts CSS style string to JS style object', function() {
         // proper css
         assert.deepEqual(
