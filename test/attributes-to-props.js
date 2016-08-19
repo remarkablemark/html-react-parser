8b1c50ead49701e9e46861c2f2d0107b7da478e6
Mark <mark@remarkablemark.org>
2016-08-19 16:46:27 -0400

    Tidy and organize tests for attributes to props helper
    Organize tests with comments and describe blocks.

--- test/attributes-to-props.js
+++ test/attributes-to-props.js
@@ -11,120 +11,134 @@ var attributesToProps = require('../lib/attributes-to-props');
  */
 describe('attributes to props helper', function() {
 
-    it('converts DOM attributes to React props', function() {
-        assert.deepEqual(
-            attributesToProps({
-                'class': 'ic',
-                'for': 'tran',
-                'http-equiv': 'refresh'
-            }),
-            {
-                className: 'ic',
-                htmlFor: 'tran',
-                httpEquiv: 'refresh',
-            }
-        );
-    });
+    /**
+     * HTML DOM property to React prop.
+     */
+    describe('HTML DOM', function() {
 
-    it('converts DOM standard properties to React props', function() {
-        assert.deepEqual(
-            attributesToProps({
-                allowfullscreen: true,
-                charset: 'utf-8',
-                tabindex: 1
-            }),
-            {
-                allowFullScreen: true,
-                charSet: 'utf-8',
-                tabIndex: 1
-            }
-        );
-    });
-
-    it('converts DOM RDFa properties to React props', function() {
-        assert.deepEqual(
-            attributesToProps({
-                property: 'foo',
-                'typeof': 'bar'
-            }),
-            {
-                property: 'foo',
-                'typeof': 'bar'
-            }
-        );
-    });
-
-    it('converts DOM non-standard properties to React props', function() {
-        assert.deepEqual(
-            attributesToProps({
-                itemscope: true,
-                itemid: 1337
-            }),
-            {
-                itemScope: true,
-                itemID: 1337
-            }
-        );
-    });
-
-    it('keeps data- and aria- attributes as is', function() {
-        assert.deepEqual(
-            attributesToProps({
-                'data-foo': 'bar',
-                'aria-live': 'polite'
-            }),
-            {
-                'data-foo': 'bar',
-                'aria-live': 'polite'
-            }
-        );
-    });
-
-    it('converts DOM attributes with weird capitalization', function() {
-        assert.deepEqual(
-            attributesToProps({
-                'ACCEPT-CHARSET': 'ISO-8859-1',
-                formNOvalidate: true,
-                sEcUrItY: 'restricted',
-                'data-FOO': 'bar'
-            }),
-            {
-                acceptCharset: 'ISO-8859-1',
-                formNoValidate: true,
-                security: 'restricted',
-                'data-FOO': 'bar'
-            }
-        );
-    });
-
-    it('converts CSS style string to JS style object', function() {
-        // proper css
-        assert.deepEqual(
-            attributesToProps({
-                style: 'color: #f00; font-size: 42px; z-index: -1;'
-            }),
-            {
-                style: {
-                    color: '#f00',
-                    fontSize: '42px',
-                    zIndex: '-1'
+        it('converts attributes to React props', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    'class': 'ic',
+                    'for': 'tran',
+                    'http-equiv': 'refresh'
+                }),
+                {
+                    className: 'ic',
+                    htmlFor: 'tran',
+                    httpEquiv: 'refresh',
                 }
-            }
-        );
+            );
+        });
 
-        // valid but messy
-        assert.deepEqual(
-            attributesToProps({
-                style: 'border-bottom-left-radius:1em;border-right-style:solid;Z-Index:-1'
-            }),
-            {
-                style: {
-                    borderBottomLeftRadius: '1em',
-                    borderRightStyle: 'solid',
-                    zIndex: '-1'
+        it('converts standard properties to React props', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    allowfullscreen: true,
+                    charset: 'utf-8',
+                    tabindex: 1
+                }),
+                {
+                    allowFullScreen: true,
+                    charSet: 'utf-8',
+                    tabIndex: 1
                 }
-            }
-        );
+            );
+        });
+
+        it('converts RDFa properties to React props', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    property: 'foo',
+                    'typeof': 'bar'
+                }),
+                {
+                    property: 'foo',
+                    'typeof': 'bar'
+                }
+            );
+        });
+
+        it('converts non-standard properties to React props', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    itemscope: true,
+                    itemid: 1337
+                }),
+                {
+                    itemScope: true,
+                    itemID: 1337
+                }
+            );
+        });
+
+        it('keeps `data-` and `aria-` attributes as is', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    'data-foo': 'bar',
+                    'aria-live': 'polite'
+                }),
+                {
+                    'data-foo': 'bar',
+                    'aria-live': 'polite'
+                }
+            );
+        });
+
+        it('converts properties with weird capitalization', function() {
+            assert.deepEqual(
+                attributesToProps({
+                    'ACCEPT-CHARSET': 'ISO-8859-1',
+                    formNOvalidate: true,
+                    sEcUrItY: 'restricted',
+                    'data-FOO': 'bar'
+                }),
+                {
+                    acceptCharset: 'ISO-8859-1',
+                    formNoValidate: true,
+                    security: 'restricted',
+                    'data-FOO': 'bar'
+                }
+            );
+        });
+
+    });
+
+    /**
+     * Style string to object.
+     */
+    describe('style', function() {
+
+        it('converts CSS style string to JS style object', function() {
+            // proper css
+            assert.deepEqual(
+                attributesToProps({
+                    style: 'color: #f00; font-size: 42px; z-index: -1;'
+                }),
+                {
+                    style: {
+                        color: '#f00',
+                        fontSize: '42px',
+                        zIndex: '-1'
+                    }
+                }
+            );
+
+            // valid but messy
+            assert.deepEqual(
+                attributesToProps({
+                    style: 'border-bottom-left-radius:1em;border-right-style:solid;Z-Index:-1'
+                }),
+                {
+                    style: {
+                        borderBottomLeftRadius: '1em',
+                        borderRightStyle: 'solid',
+                        zIndex: '-1'
+                    }
+                }
+            );
+        });
+
     });
 
 });
