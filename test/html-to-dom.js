26213ba2c40f1bcbda3b3efc8e8fbb61cc7525a4
Mark <mark@remarkablemark.org>
2016-08-22 16:05:04 -0400

    Add tests for HTML to DOM parsing on the client
    - Organize Mocha tests to differentiate between environments
      (node versus browser)
    - Add tests to confirm `formatDOM` (client) works as expected and
      returns the same output as `parseDOM` (server)
    - Update HTML mocks with more cases
    - Save `jsdomify` to devDependencies.

--- test/html-to-dom.js
+++ test/html-to-dom.js
@@ -5,22 +5,58 @@
  */
 var assert = require('assert');
 var util = require('util');
-var parseDOM = require('../lib/html-to-dom');
+var jsdomify = require('jsdomify').default;
 var htmlparser = require('htmlparser2');
+var parseDOM = require('../lib/html-to-dom');
+var formatDOM = require('../lib/html-to-dom-client');
+var helpers = require('./helpers/');
 var data = require('./data');
 
 /**
- * Tests for `parseDOM`.
+ * Tests for HTML to DOM parser.
  */
-describe('html-to-dom parser', function() {
+describe('html-to-dom', function() {
 
-    it('works the same as `require("htmlparser2").parseDOM`', function() {
-        var html = data.html.complex;
-        // use `util.inspect` to resolve circular references
-        assert.equal(
-            util.inspect(parseDOM(html), { showHidden: true, depth: null }),
-            util.inspect(htmlparser.parseDOM(html), { showHidden: true, depth: null })
-        );
+    /**
+     * Node environment.
+     */
+    describe('server parser', function() {
+
+        it('works the same as `require("htmlparser2").parseDOM`', function() {
+            var html = data.html.complex;
+            // use `util.inspect` to resolve circular references
+            assert.equal(
+                util.inspect(parseDOM(html), { showHidden: true, depth: null }),
+                util.inspect(htmlparser.parseDOM(html), { showHidden: true, depth: null })
+            );
+        });
+
+    });
+
+    /**
+     * Browser environment.
+     */
+    describe('client parser', function() {
+
+        // setup mock browser environment
+        before(function() {
+            jsdomify.create();
+        });
+
+        // teardown mock browser environment
+        after(function() {
+            jsdomify.destroy();
+        });
+
+        it('formats the nodes like `htmlparser2.parseDOM`', function() {
+            var html = data.html.attributes + data.html.nested + data.html.comment + data.html.script;
+            var root = document.createElement('div');
+            root.innerHTML = html;
+            var clientNodes = formatDOM(root.childNodes);
+            var serverNodes = parseDOM(html);
+            helpers.deepEqualCircular(clientNodes, serverNodes);
+        });
+
     });
 
 });
