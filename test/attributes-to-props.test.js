65580d369b26360f625e5d958a256caad5fb57e8
Mark <mark@remarkablemark.org>
2020-12-06 17:48:13 -0500

    test(attributes-to-props): add cases and refactor to snapshots

--- test/attributes-to-props.test.js
+++ test/attributes-to-props.test.js
@@ -1,241 +1,202 @@
 const attributesToProps = require('../lib/attributes-to-props');
 const utilities = require('../lib/utilities');
 
-describe('attributes to props', () => {
-  describe('HTML', () => {
-    it('converts attributes to React props', () => {
-      expect(
-        attributesToProps({
-          class: 'ic',
-          for: 'tran',
-          'http-equiv': 'refresh'
-        })
-      ).toEqual({
-        className: 'ic',
-        htmlFor: 'tran',
-        httpEquiv: 'refresh'
-      });
-    });
-
-    it('converts standard attributes to React props', () => {
-      expect(
-        attributesToProps({
-          allowfullscreen: true,
-          charset: 'utf-8',
-          tabindex: 1
-        })
-      ).toEqual({
-        allowFullScreen: true,
-        charSet: 'utf-8',
-        tabIndex: 1
-      });
-    });
-
-    it('converts RDFa attributes to React props', () => {
-      expect(
-        attributesToProps({
-          property: 'foo',
-          typeof: 'bar'
-        })
-      ).toEqual({
-        property: 'foo',
-        typeof: 'bar'
-      });
-    });
-
-    it('converts non-standard attributes to React props', () => {
-      expect(
-        attributesToProps({
-          itemscope: true,
-          itemid: 1337
-        })
-      ).toEqual({
-        itemScope: true,
-        itemID: 1337
-      });
-    });
-
-    it('keeps `data-*` and `aria-*` attributes as is', () => {
-      expect(
-        attributesToProps({
-          'data-foo': 'bar',
-          'aria-live': 'polite'
-        })
-      ).toEqual({
-        'data-foo': 'bar',
-        'aria-live': 'polite'
-      });
-    });
-
-    it('converts attributes with weird capitalization', () => {
-      expect(
-        attributesToProps({
-          'ACCEPT-CHARSET': 'ISO-8859-1',
-          formNOvalidate: true,
-          sEcUrItY: 'restricted',
-          'data-FOO': 'bar'
-        })
-      ).toEqual({
-        acceptCharset: 'ISO-8859-1',
-        formNoValidate: true,
-        security: 'restricted',
-        'data-FOO': 'bar'
-      });
-    });
-
-    it('converts boolean attributes', () => {
-      expect(
-        attributesToProps({
-          readonly: ''
-        })
-      ).toEqual({
-        readOnly: true
-      });
-
-      expect(
-        attributesToProps({
-          disabled: 'disabled'
-        })
-      ).toEqual({
-        disabled: true
-      });
-    });
-
-    it('converts overloaded boolean attributes', () => {
-      expect(
-        attributesToProps({
-          download: ''
-        })
-      ).toEqual({
-        download: true
-      });
-
-      expect(
-        attributesToProps({
-          download: 'filename'
-        })
-      ).toEqual({
-        download: 'filename'
-      });
-    });
+describe('attributesToProps with HTML attribute', () => {
+  it('converts attributes to React props', () => {
+    const attributes = {
+      class: 'ic',
+      for: 'tran',
+      'http-equiv': 'refresh'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
   });
 
-  describe('SVG', () => {
-    it('converts attributes to React props', () => {
-      expect(
-        attributesToProps({
-          edgeMode: 'edgeMode',
-          'fill-opacity': '0.42',
-          'fill-rule': 'evenodd',
-          'glyph-orientation-vertical': 'auto',
-          'horiz-adv-x': '9001',
-          stroke: 'none',
-          'xml:base': 'http://example.org'
-        })
-      ).toEqual({
-        edgeMode: 'edgeMode',
-        fillOpacity: '0.42',
-        fillRule: 'evenodd',
-        glyphOrientationVertical: 'auto',
-        horizAdvX: '9001',
-        stroke: 'none',
-        xmlBase: 'http://example.org'
-      });
-    });
-
-    it('keeps incorrectly capitalized attributes', () => {
-      expect(
-        attributesToProps({
-          'XLINK:HREF': '#',
-          YChannelSelector: 'G',
-          ZoomAndPan: 'disable'
-        })
-      ).toEqual({
-        'XLINK:HREF': '#',
-        YChannelSelector: 'G',
-        ZoomAndPan: 'disable'
-      });
-    });
+  it('converts standard attributes to React props', () => {
+    const attributes = {
+      allowfullscreen: true,
+      charset: 'utf-8',
+      tabindex: 1
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
   });
 
-  // cssToJs
-  describe('style', () => {
-    const propsEmptyStyle = { style: {} };
+  it('converts RDFa attributes to React props', () => {
+    const attributes = {
+      property: 'foo',
+      typeof: 'bar'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
 
-    it.each([undefined, null])('does not parse invalid value: %s', style => {
-      expect(attributesToProps({ style })).toEqual({ style });
-    });
+  it('converts non-standard attributes to React props', () => {
+    const attributes = {
+      itemscope: true,
+      itemid: 1337
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
 
-    it('does not parse CSS comment', () => {
-      const style = '/* comment */';
-      expect(attributesToProps({ style })).toEqual(propsEmptyStyle);
-    });
+  it('keeps `data-*` and `aria-*` attributes as is', () => {
+    const attributes = {
+      'data-foo': 'bar',
+      'aria-live': 'polite'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
 
-    it('parses "" to {}', () => {
-      const style = '';
-      expect(attributesToProps({ style })).toEqual(propsEmptyStyle);
-    });
+  it('converts attributes with weird capitalization', () => {
+    const attributes = {
+      'ACCEPT-CHARSET': 'ISO-8859-1',
+      formNOvalidate: true,
+      sEcUrItY: 'restricted',
+      'data-FOO': 'bar'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
 
-    it('parses CSS to JS', () => {
-      const style = `
+  /**
+   * The presence of a boolean attribute on an element represents the “true” value, and the absence of the attribute represents the “false” value.
+   * The values “true” and “false” are not allowed on boolean attributes.
+   * To represent a false value, the attribute has to be omitted altogether.
+   *
+   * @see {@link https://meiert.com/en/blog/boolean-attributes-of-html/}
+   */
+  it('converts boolean attributes', () => {
+    const attributes = {
+      allowfullscreen: '',
+      allowpaymentrequest: '',
+      async: 'false',
+      autofocus: 'true',
+      autoplay: 'true',
+      checked: 'true',
+      controls: '',
+      default: '',
+      disabled: 'disabled',
+      formnovalidate: 'true',
+      hidden: 'true',
+      ismap: '',
+      itemscope: '',
+      loop: '',
+      multiple: '',
+      muted: '',
+      nomodule: '',
+      novalidate: '',
+      open: '',
+      playsinline: '',
+      readonly: '',
+      required: '',
+      reversed: '',
+      selected: '',
+      truespeed: ''
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
+
+  it.each([
+    [{ download: '' }, { download: true }],
+    [{ download: 'filename' }, { download: 'filename' }]
+  ])('converts overloaded boolean attribute: %p', (attributes, props) => {
+    expect(attributesToProps(attributes)).toEqual(props);
+  });
+});
+
+describe('attributesToProps with SVG attribute', () => {
+  it('converts attributes to React props', () => {
+    const attributes = {
+      edgeMode: 'edgeMode',
+      'fill-opacity': '0.42',
+      'fill-rule': 'evenodd',
+      'glyph-orientation-vertical': 'auto',
+      'horiz-adv-x': '9001',
+      stroke: 'none',
+      'xml:base': 'http://example.org'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
+
+  it('keeps incorrectly capitalized attributes', () => {
+    const attributes = {
+      'XLINK:HREF': '#',
+      YChannelSelector: 'G',
+      ZoomAndPan: 'disable'
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
+});
+
+describe('attributesToProps with style attribute', () => {
+  const propsEmptyStyle = { style: {} };
+
+  it.each([undefined, null])('does not parse invalid value: %s', style => {
+    expect(attributesToProps({ style })).toEqual({ style });
+  });
+
+  it('skips CSS comment', () => {
+    const style = '/* comment */';
+    expect(attributesToProps({ style })).toEqual(propsEmptyStyle);
+  });
+
+  it('parses empty string to empty style object', () => {
+    const style = '';
+    expect(attributesToProps({ style })).toEqual(propsEmptyStyle);
+  });
+
+  it('parses CSS style to JS object', () => {
+    const style = `
         color: #f00; font-size: 42px; z-index: -1; -webkit-border-top-right-radius: 10rem; background: url(data:image/png; base64,ivborw0kggoaaaansaaaabgdbtueaalgpc/xhbqaaaafzmuexurczmzpf399fx1+bm5mzy9avzxbesmgces5/p8/t9furvcrmu73jwlzosgsiizurcjo/ad+eqjjb4hv8bft+idpqocx1wjosbfhh2xssxeiyn3uli/6mnree07uiwjev8u8czwyuqdlkpg1bkb4nnm+veanfhqn1k4+gpt6ugqcvu2h2ovuif);
         /* display: block;  */
         --custom-property: #f00;
         border-bottom-left-radius:1em;border-right-style:solid;Z-Index:-1;-moz-border-radius-bottomleft:20px'
       `;
-      expect(attributesToProps({ style })).toMatchSnapshot();
-    });
+    expect(attributesToProps({ style })).toMatchSnapshot();
+  });
+});
+
+describe('attributesToProps with custom attribute', () => {
+  it('converts attributes named after Object properties', () => {
+    const attributes = {
+      __defineGetter__: '',
+      __defineSetter__: '',
+      __lookupGetter__: '',
+      __lookupSetter__: '',
+      __proto__: '',
+      constructor: '',
+      hasOwnProperty: '',
+      isPrototypeOf: '',
+      propertyIsEnumerable: '',
+      toLocaleString: '',
+      toString: '',
+      valueOf: ''
+    };
+    expect(attributesToProps(attributes)).toMatchSnapshot();
+  });
+});
+
+describe('utilities.PRESERVE_CUSTOM_ATTRIBUTES=false', () => {
+  const { PRESERVE_CUSTOM_ATTRIBUTES } = utilities;
+  const emptyProps = {};
+
+  beforeAll(() => {
+    utilities.PRESERVE_CUSTOM_ATTRIBUTES = false;
   });
 
-  describe('custom', () => {
-    it('converts attributes named after Object properties', () => {
-      // handled as custom attributes
-      const attributes = {
-        __defineGetter__: '',
-        __defineSetter__: '',
-        __lookupGetter__: '',
-        __lookupSetter__: '',
-        __proto__: '',
-        constructor: '',
-        hasOwnProperty: '',
-        isPrototypeOf: '',
-        propertyIsEnumerable: '',
-        toLocaleString: '',
-        toString: '',
-        valueOf: ''
-      };
+  afterAll(() => {
+    utilities.PRESERVE_CUSTOM_ATTRIBUTES = PRESERVE_CUSTOM_ATTRIBUTES;
+  });
 
-      expect(attributesToProps(attributes)).toEqual(attributes);
-    });
+  it('omits unknown attributes', () => {
+    const attributes = {
+      unknownAttribute: 'someValue'
+    };
+    expect(attributesToProps(attributes)).toEqual(emptyProps);
+  });
 
-    describe('when utilties.PRESERVE_CUSTOM_ATTRIBUTES=false', () => {
-      const { PRESERVE_CUSTOM_ATTRIBUTES } = utilities;
-
-      beforeAll(() => {
-        utilities.PRESERVE_CUSTOM_ATTRIBUTES = false;
-      });
-
-      afterAll(() => {
-        utilities.PRESERVE_CUSTOM_ATTRIBUTES = PRESERVE_CUSTOM_ATTRIBUTES;
-      });
-
-      it('omits unknown attributes', () => {
-        expect(
-          attributesToProps({
-            unknownAttribute: 'someValue'
-          })
-        ).toEqual({});
-      });
-
-      it('omits incorrectly capitalized attributes', () => {
-        expect(
-          attributesToProps({
-            'XLINK:HREF': '#',
-            YChannelSelector: 'G',
-            ZoomAndPan: 'disable'
-          })
-        ).toEqual({});
-      });
-    });
+  it('omits incorrectly capitalized attributes', () => {
+    const attributes = {
+      'XLINK:HREF': '#',
+      YChannelSelector: 'G',
+      ZoomAndPan: 'disable'
+    };
+    expect(attributesToProps(attributes)).toEqual(emptyProps);
   });
 });
