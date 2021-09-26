42f42e519c05c382476a14a73d45b5f700fecdbb
Stefan Natter <stefan@natter.at>
2021-09-26 16:11:45 +0200

    feat: added CRA typescript example

---
+++ examples/create-react-app-typescript/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import parse, { domToReact, htmlToDOM, Element } from 'html-react-parser';
+import './App.css';
+
+console.log(domToReact);
+console.log(htmlToDOM);
+
+const parser = (input: string) =>
+  parse(input, {
+    replace: domNode => {
+      if (domNode instanceof Element && domNode.attribs.class === 'remove') {
+        return <></>;
+      }
+    }
+  });
+
+export default function App() {
+  return (
+    <div className="App">
+      {parser(`
+          <h2 style="font-family: 'Lucida Grande';">
+            HTMLReactParser<br class="remove"> loaded withCreate React App
+          </h2>
+        `)}
+    </div>
+  );
+}
