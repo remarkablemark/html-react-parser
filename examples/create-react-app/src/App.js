a0e4461523a7ea1ecc91962d274f647b0f1706aa
Stefan Natter <stefan@natter.at>
2021-09-26 16:14:18 +0200

    chore: updated CRA (javascript) and align it with the TypeScript example

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import parse, { domToReact, htmlToDOM } from 'html-react-parser';
+import parse, { domToReact, htmlToDOM, Element } from 'html-react-parser';
 import './App.css';
 
 console.log(domToReact);
 console.log(htmlToDOM);
 
+const parser = input =>
+  parse(input, {
+    replace: domNode => {
+      if (domNode instanceof Element && domNode.attribs.class === 'remove') {
+        return <></>;
+      }
+    }
+  });
+
 export default function App() {
   return (
     <div className="App">
-      {parse(`
-        <h2 style="font-family: 'Lucida Grande';">
-          HTMLReactParser loaded with Create React App
-        </h2>
-      `)}
+      {parser(`
+            <h2 style="font-family: 'Lucida Grande';">
+              HTMLReactParser<br class="remove"> loaded withCreate React App
+            </h2>
+          `)}
     </div>
   );
 }
