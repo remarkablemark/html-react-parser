bb4bf49b150be992917958813a7b17d1d7cef31c
Adam Močkoř <adam.mockor@gmail.com>
2023-01-05 23:44:10 +0100

    fix(attributes-to-props): don't convert value of option tag (#790)
    * fix(attributes-to-props): don't convert value of option tag
    
    Fixes #625
    
    * fix(attributes-to-props): fix issues with tests
    
    * fix(attributes-to-props): increase size limit
    
    * fix(attributes-to-props): improve jsdoc documentation
    
    * fix(attributes-to-props): remove lock file
    
    * fix(attributes-to-props): fix type issues

--- lib/attributes-to-props.d.ts
+++ lib/attributes-to-props.d.ts
@@ -10,6 +10,10 @@ export type Props = Record<string, string> & {
  * Converts HTML/SVG DOM attributes to React props.
  *
  * @param attributes - HTML/SVG DOM attributes.
+ * @param nodeName - DOM node name.
  * @returns - React props.
  */
-export default function attributesToProps(attributes: Attributes): Props;
+export default function attributesToProps(
+  attributes: Attributes,
+  nodeName?: string
+): Props;
