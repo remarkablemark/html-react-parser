76f5afc65690965d8ac234233d64737f3a2544ef
Mark <mark@remarkablemark.org>
2023-10-28 11:16:15 -0400

    build(examples): upgrade create-react-app-typescript devDependencies

--- examples/create-react-app-typescript/src/index.tsx
+++ examples/create-react-app-typescript/src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
