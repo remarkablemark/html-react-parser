9fa9faa34b90be942071b74ffb6cd827c668282d
Mark <mark@remarkablemark.org>
2023-10-29 10:40:03 -0400

    refactor(examples): tidy create-react-app

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,26 +1,30 @@
 import parse, { domToReact, htmlToDOM, Element } from 'html-react-parser';
+
 import './App.css';
 
 console.log(domToReact);
 console.log(htmlToDOM);
 
-const parser = input =>
-  parse(input, {
-    replace: domNode => {
-      if (domNode instanceof Element && domNode.attribs.class === 'remove') {
-        return <></>;
-      }
-    }
-  });
-
 export default function App() {
   return (
     <div className="App">
-      {parser(`
+      {parse(
+        `
         <h2 style="font-family: 'Lucida Grande';">
           HTMLReactParser<br class="remove"> with Create React App
         </h2>
-      `)}
+        `,
+        {
+          replace(domNode) {
+            if (
+              domNode instanceof Element &&
+              domNode.attribs.class === 'remove'
+            ) {
+              return <></>;
+            }
+          }
+        }
+      )}
     </div>
   );
 }
