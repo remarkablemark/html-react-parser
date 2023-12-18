3e0fd2b31ad602e3f791fef22ec5cd220aa4dd74
Mark <mark@remarkablemark.org>
2023-12-18 12:24:17 -0500

    fix(esm): make ESM exports explicit in index.mjs and utilities.mjs
    Fixes #1228
    
    Vite fails with the error:
    
    ```
    Uncaught SyntaxError: The requested module '/@fs/Users/.../node_modules/.vite/app/deps/html-react-parser.js?v=719515aa' does not provide an export named 'Element' (at list.tsx:6:31)
    ```

--- esm/index.mjs
+++ esm/index.mjs
@@ -1,5 +1,13 @@
 import HTMLReactParser from '../lib/index.js';
 
-export * from '../lib/index.js';
+export {
+  Comment,
+  Element,
+  ProcessingInstruction,
+  Text,
+  attributesToProps,
+  domToReact,
+  htmlToDOM,
+} from '../lib/index.js';
 
 export default HTMLReactParser.default || HTMLReactParser;
